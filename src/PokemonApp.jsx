fb0af71a1a1290a720875212ad305be21d88e073
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-22 18:43:38 -0500

    thunks

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { getPokemons } from "./store/slices/pokemon/thunks"
 
 export const PokemonApp = () => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+      dispatch( getPokemons() )
+    
+    }, [])
+    
+
     return (
         <>
             <h1>PokemonApp</h1>
